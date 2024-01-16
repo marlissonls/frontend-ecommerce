9ee1d37dda2e74bc223423eb5e56ca100beaefe0
Marlisson Silva <marlisson.business@gmail.com>
2024-01-16 14:07:04 -0300

    feat: definição da página de pesquisa

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
- export default class Pesquisa extends Component {
-     render() {
-         return (
-             <div>
-                 <h2>Loja IT - Pesquisa</h2>
-             </div>
-         )
-     }
- }
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import ProdutosPesquisa from '../containers/Lista/ProdutosPesquisa';
+import Rodape from '../containers/Rodape';
+
+export default class Pesquisa extends Component {
+    render() {
+        return (
+            <Layout title='Resultados para Mouse | LOJA IT - Melhores produtos de Informática'>
+                <Cabecalho />
+                <ProdutosPesquisa />
+                <Rodape />
+            </Layout>
+        )
+    }
+}
